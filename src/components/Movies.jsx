a87d567cd93daa0c4a636ef29c92be0082d7f1ff
Ayush <ayushverma1016@gmail.com>
2024-09-03 10:53:28 +0530

    Commit message

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,58 +1,98 @@
-import React, { useState } from 'react'
-import {movies} from './data.js'
+// import React, { useState } from 'react'
+// import {movies} from './data.js'
+
+// const Movies = () => {
+//   const [movieList, setMovieList] = useState(movies)
+
+//   const filterByCategory = (cat)=>{
+//     setMovieList(movies.filter((data)=>data.category==cat))
+//   }
+
+//   return (
+//     <>
+  
+//     <div className='my-3 text-center'  style={{width:'1000px',margin:'auto'}}>
+//       <button onClick={()=>setMovieList(movies)} type="button" className="btn btn-outline-primary mx-3">All</button>
+//       <button onClick={()=>filterByCategory("Action")} type="button" className="btn btn-outline-secondary mx-3">Action</button>
+//       <button onClick={()=>filterByCategory("Thriller")} type="button" className="btn btn-outline-success mx-3">Thriller</button>
+//       <button onClick={()=>filterByCategory("Animation")} type="button" className="btn btn-outline-danger mx-3">Animation</button>
+//       <button onClick={()=>filterByCategory("Horror")} type="button" className="btn btn-outline-warning mx-3">Horror</button>
+//       <button onClick={()=>filterByCategory("Drama")} type="button" className="btn btn-outline-info mx-3">Drama</button>
+//       <button onClick={()=>filterByCategory("Sci-Fi")} type="button" className="btn btn-outline-secondary mx-3">Sci-Fi</button>
+//     </div>
+
+//       <div
+//     style={{
+//       display:'flex',
+//       justifyContent:'center',
+//       alignItems:'center',
+//       flexWrap:'wrap',
+//       // gap:'32px',
+//       textAlign:'center',
+//       width:'1300px',
+//       margin:'auto',
+//       marginTop:'1.5rem'
+//       }}>
+//       {movieList.map((data)=>(<div key={data.id}  style={{maxWidth:'280px',textAlign:'center'}}>
+//         <div style={{padding:'10px',}} className='hover_effect'> 
+//           <img src={data.poster_path} alt="" style={{width:'200px',height:'280px',borderRadius:'10px',border:'3px solid orange'}}/>
+//         </div>
+//         <h5>{data.title}</h5>
+//         <p>{data.release_date}</p>
+//       </div>))}
+//     </div>
+
+
+//     </>
+    
+//   )
+// }
+
+// export default Movies
+
+
+
+
+
+// -----------------------------------------
+
+import React, { useState } from 'react';
+import { movies } from './data.js';
 
 const Movies = () => {
-  const [movieList, setMovieList] = useState(movies)
+  const [movieList, setMovieList] = useState(movies);
 
-  const filterByCategory = (cat)=>{
-    setMovieList(movies.filter((data)=>data.category==cat))
-  }
+  const filterByCategory = (cat) => {
+    setMovieList(movies.filter((data) => data.category === cat));
+  };
 
   return (
     <>
-  
-    <div className='my-3 text-center' style={{width:'1000px',margin:'auto'}}>
-      <button onClick={()=>setMovieList(movies)} type="button" className="btn btn-outline-primary mx-3">All</button>
-      <button onClick={()=>filterByCategory("Action")} type="button" className="btn btn-outline-secondary mx-3">Action</button>
-      <button onClick={()=>filterByCategory("Thriller")} type="button" className="btn btn-outline-success mx-3">Thriller</button>
-      <button onClick={()=>filterByCategory("Animation")} type="button" className="btn btn-outline-danger mx-3">Animation</button>
-      <button onClick={()=>filterByCategory("Horror")} type="button" className="btn btn-outline-warning mx-3">Horror</button>
-      <button onClick={()=>filterByCategory("Drama")} type="button" className="btn btn-outline-info mx-3">Drama</button>
-      <button onClick={()=>filterByCategory("Sci-Fi")} type="button" className="btn btn-outline-secondary mx-3">Sci-Fi</button>
-    </div>
+      <div className='my-3 text-center' style={{ maxWidth: '1000px', margin: 'auto', padding: '0 20px' }}>
+        <button onClick={() => setMovieList(movies)} type="button" className="btn btn-outline-primary mx-2">All</button>
+        <button onClick={() => filterByCategory("Action")} type="button" className="btn btn-outline-secondary mx-2">Action</button>
+        <button onClick={() => filterByCategory("Thriller")} type="button" className="btn btn-outline-success mx-2">Thriller</button>
+        <button onClick={() => filterByCategory("Animation")} type="button" className="btn btn-outline-danger mx-2">Animation</button>
+        <button onClick={() => filterByCategory("Horror")} type="button" className="btn btn-outline-warning mx-2">Horror</button>
+        <button onClick={() => filterByCategory("Drama")} type="button" className="btn btn-outline-info mx-2">Drama</button>
+        <button onClick={() => filterByCategory("Sci-Fi")} type="button" className="btn btn-outline-secondary mx-2">Sci-Fi</button>
+      </div>
 
-      <div
-    style={{
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      flexWrap:'wrap',
-      // gap:'32px',
-      textAlign:'center',
-      width:'1300px',
-      margin:'auto',
-      marginTop:'1.5rem'
-      }}>
-      {movieList.map((data)=>(<div key={data.id}  style={{maxWidth:'280px',textAlign:'center'}}>
-        <div style={{padding:'10px',}} className='hover_effect'> 
-          <img src={data.poster_path} alt="" style={{width:'200px',height:'280px',borderRadius:'10px',border:'3px solid orange'}}/>
-        </div>
-        <h5>{data.title}</h5>
-        <p>{data.release_date}</p>
-      </div>))}
-    </div>
-
-
+      <div className='movies-container'>
+        {movieList.map((data) => (
+          <div key={data.id} className='movie-item'>
+            <div className='hover_effect'>
+              <img src={data.poster_path} alt="" />
+            </div>
+            <h5>{data.title}</h5>
+            <p>{data.release_date}</p>
+          </div>
+        ))}
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Movies
-
-
-
-
-
+export default Movies;
 
 
